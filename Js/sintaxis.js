80abdee494bad2b5012c0b460d4db455a2a65420
Joel Campos <joelcamposcai2456@gmail.com>
2022-12-23 17:49:38 -0300

    Segundo Versionado

---
+++ Js/sintaxis.js
@@ -0,0 +1,73 @@
+/*
+let numero = 20;
+let apellido = "Campos";
+let objeto = {nombre: "joel", apellido: "Campos"}; 
+*/
+
+/*
+//Sobreescritura de tipo de variable
+let noTipado;
+noTipado = 5;
+noTipado = "persona";
+*/
+
+/*
+var mayoriaDeEdad = 20;
+var edadPersona =35;
+
+if (edadPersona > mayoriaDeEdad)
+{
+    console.log("hacer esto");
+}
+else
+{
+    console.log("Hacer lo otro");
+} 
+*/
+
+
+/*
+var contador = 0;
+var limite = 5;
+*/
+/* iteración es lo que esta dentro de las llaves que se ejecutará 
+   varias veces mientras la condición (la del medio) sea verdadera */
+/* El primer elemento del for será un acumulador*/ 
+/* El tercer elemento del for será un contador*/
+/*     
+for (contador; contador <= limite; contador++)
+{
+    console.log("el valor del contador es : " + contador );
+}
+*/
+
+/*
+// Método Saludo
+function Saludo ()
+{
+    console.log("Hola");
+}
+
+Saludo();
+
+function Sumar (a,b)
+{
+    var resultadoSuma = a + b;
+    return resultadoSuma;
+}
+
+var suma = Sumar(3,4)
+console.log(suma);
+*/
+
+let nombre;
+nombre = "hola";
+console.log(nombre);
+nombre = 5;
+console.log(nombre);
+
+var hola;
+hola = "uno";
+console.log(hola);
+hola = 4;
+console.log(hola);
