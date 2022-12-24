4b60ed26e809898fec74a14699076f3d54bd16a8
Joel Campos <joelcamposcai2456@gmail.com>
2022-12-24 03:14:30 -0300

    Cuarto Versionado

--- Js/javascript.js
+++ Js/javascript.js
@@ -3,3 +3,24 @@ document.getElementById('botonCambioTexto').onclick = function ()
    console.log("Inicia el cambio de Texto");
    document.getElementById("parrafoUno").innerHTML = "Texto Cambiado"; 
 }
+
+document.getElementById('botonCambioColor').addEventListener ('click',
+function ()
+{  
+   document.body.style.backgroundColor = '#EFFD5F';
+})
+
+document.getElementById('botonRestablecerColor').addEventListener ('click',
+function ()
+{
+   document.body.style.backgroundColor = '#FFFFFF';
+})
+
+
+const colleccionVerde = document.getElementsByClassName("parrafoVerde");
+let contadorUno = 0;
+for (contadorUno;contadorUno < colleccionVerde.length;contadorUno++)
+{
+   colleccionVerde[contadorUno].style.backgroundColor = '#06F727';
+}
+
