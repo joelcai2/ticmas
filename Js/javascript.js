3460817ac2cf0ab6fa715b6208023bddc4d4829e
Joel Campos <joelcamposcai2456@gmail.com>
2022-12-24 16:44:01 -0300

    Quinto Versionado

--- Js/javascript.js
+++ Js/javascript.js
@@ -24,3 +24,26 @@ for (contadorUno;contadorUno < colleccionVerde.length;contadorUno++)
    colleccionVerde[contadorUno].style.backgroundColor = '#06F727';
 }
 
+document.getElementById('suma').addEventListener('click',
+function ()
+{
+   let primerNumero = document.getElementById('numeroUno').value;
+   console.log("El primer número ingresado es:" + primerNumero);
+
+   let segundoNumero = document.getElementById('numeroDos').value;
+   console.log("El primer número ingresado es:" + segundoNumero);
+
+   let resultadoSuma = suma(primerNumero,segundoNumero);
+   console.log("El resultado de la suma es:" + resultadoSuma);
+
+   document.getElementById('resultadoSuma').innerHTML = resultadoSuma;
+   document.getElementById('contenedorResultado').style.display = 'block';
+
+})
+
+function suma (numeroUno,numeroDos)
+{
+   let resultadoSuma = parseInt(numeroUno) + parseInt(numeroDos);
+   return resultadoSuma;
+
+}
